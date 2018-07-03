0488d8b7837da6fe5039c0cee36ff4f4a1a5109c
DK <Donovan.Daniels50@gmail.com>
2018-07-03 13:49:40 +0200

    Commit all files

--- src/ContainerComponents/Multiplier.js
+++ src/ContainerComponents/Multiplier.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import MultiplierDisplay from '../StyleComponents/MultiplierDisplay';
 import {connect} from 'react-redux';
-import {multiplyAction, divideAction} from '../Redux/actions/multiplyActions';
+import {multiplyAction, divideAction, changecolorAction} from '../Redux/actions/multiplyActions';
 
 class Multiplier extends React.Component {
+
+   
+
     render() {
         console.log(this.props);
-        const { number, handleClick, handleClick2 } = this.props;
+        const { number, handleClick, handleClick2, changeColor, changeLanguage } = this.props;
+            
+ 
         return (
-        <MultiplierDisplay number={number} handleClick={handleClick} handleClick2={handleClick2}/>
+        <div> <MultiplierDisplay number={number} handleClick={handleClick} handleClick2={handleClick2}
+        changeColor={changeColor} color={this.props.color} changeDisplay={this.props.changeDisplay} 
+        display={this.props.display} changeLanguage={changeLanguage} language={this.props.language}/>
+        </div>
     )
     }
 };
 
 const mapStateToProps = (state) => {
-    return { number: state.multiplyReducer.number}
+    return { 
+        number: state.multiplyReducer.number,
+        color: state.renderReducer.color,
+        display: state.renderReducer.display,
+        language: state.renderReducer.language,
+    }
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -24,7 +37,16 @@ const mapDispatchToProps = (dispatch) => {
         },
         handleClick2: (event) => {
             dispatch(divideAction)
-        }
+        },
+        changeColor: (event) => {
+            dispatch(changecolorAction)
+        },
+        changeDisplay: (event) => {
+            dispatch({ type: 'CHANGE_DISPLAY'})
+        },
+        changeLanguage: (event) => {
+            dispatch({ type: 'CHANGE_LANGUAGE'})
+        },
     }
 }
 
